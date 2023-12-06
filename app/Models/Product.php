db7a43d089698b97430da4a2376557ee8bde60e0
JosephHuy1906 <103304480+JosephHuy1906@users.noreply.github.com>
2023-12-06 17:17:21 +0700

    done task (#8)

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,6 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'categoryID');
+    }
     protected $fillable = [
         'name',
         'categoryID',
